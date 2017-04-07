e4ab8f03f48afa2f1b5c0e23032416c718497f57
Mohamad Khan <mohamadkhan19@gmail.com>
2017-04-07 16:12:34 -0700

    controller added

--- app/scripts/controllers/chartContoller.js
+++ app/scripts/controllers/chartContoller.js
@@ -6,8 +6,10 @@
  * # MainCtrl
  * Controller of the sbAdminApp
  */
+
+
 angular.module('sbAdminApp')
-  .controller('ChartCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+  .controller('ChartCtrl', ['$scope', '$http' , '$timeout', function ($scope, $http, $timeout) {
     $scope.line = {
 	    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
 	    series: ['Series A', 'Series B'],
@@ -20,16 +22,46 @@ angular.module('sbAdminApp')
 	    }
     };
 
-    $scope.bar = {
-	    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-		series: ['Series A', 'Series B'],
+    $scope.bar_function = function() {
+        //Get sensors type
+        //var data = APP_CLOUD.getHeaders(true);
+        $http({
+          method: 'GET',
+          url: 'http://localhost:4300/customers'
+        }).then(function(resp) {
+          console.log("This is response: "+resp.data.obj[1].sumofproducts);
+                $scope.totalPrice = resp.data;
+                var temp1 = [ ];
+                var temp2 = [ ];
+                $scope.lables = [];
+                $scope.data = [];
+                angular.forEach($scope.totalPrice.obj,function(item){
+                   temp1.push(item.year);
+                   temp2.push(item.sumofproducts);
+               });
 
-		data: [
-		   [65, 59, 80, 81, 56, 55, 40],
-		   [28, 48, 40, 19, 86, 27, 90]
-		]
-    	
+            
+            return  $scope.bar = {
+                labels : temp1,
+                data : [temp2],
+                series: ['Series A']
+            };
+        });
+
+
     };
+
+
+    /*$scope.bar = {
+        labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+        series: ['Series A', 'Series B'],
+
+        data: [
+           [65, 59, 80, 81, 56, 55, 40],
+           [28, 48, 40, 19, 86, 27, 90]
+        ]
+        
+    };*/
 
     $scope.donut = {
     	labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
