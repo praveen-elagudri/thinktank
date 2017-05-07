da7affb7c82f1e8b4bc542ae6dd7861c53500563
praveen-elagudri <elagudripraveen@gmail.com>
2017-05-06 22:05:42 -0700

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 -#!/bin/sh
 ssh ubuntu@ip-172-31-15-68 <<EOF
- cd /praveen/nodeLatest/  
- git pull       
- npm start
+cd /home/ubuntu/praveen/thinktank 
+sudo git pull       
+sudo npm start&
  exit       
 EOF
